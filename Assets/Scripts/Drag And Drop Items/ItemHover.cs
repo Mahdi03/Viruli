7db05b94c5d7abd1d3a5be4c410ad451adbbf364
Mahdi Ayman <damahdi03@gmail.com>
2022-11-11 15:30:05 -0800

    Added script for floating effect on pick up sprites

---
+++ Assets/Scripts/Drag And Drop Items/ItemHover.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemHover : MonoBehaviour {
+    private float mainY = 0, /*rotationSpeed = 35f,*/ timeElasped = 0f;
+    private RectTransform rectTransform;
+    // Start is called before the first frame update
+    void Start() {
+        rectTransform = GetComponent<RectTransform>();
+        mainY = rectTransform.position.y;
+    }
+
+	// Update is called once per frame
+	void Update() {
+        timeElasped += Time.deltaTime;
+        /*
+        //Rotate indefinitely
+        var angle = transform.rotation.eulerAngles;
+        angle.y += Time.deltaTime * rotationSpeed;
+        transform.rotation = Quaternion.Euler(angle);
+        */
+        //Bobbing effect
+        var pos = rectTransform.position;
+        pos.y = mainY + 20f * Mathf.Sin(2 * timeElasped);
+        rectTransform.position = pos;
+    }
+}
