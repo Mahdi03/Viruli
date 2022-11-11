b6f690cf0ff19b7c32f70b0a53cf57f59c266878
Mahdi Ayman <damahdi03@gmail.com>
2022-11-11 15:30:28 -0800

    Creating scripts to attach data to the actual prefab instances for calling later

---
+++ Assets/Scripts/Attached Data Scripts/AttachedRawMaterialData.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachedRawMaterialData : AttachedItemData {
+    public bool droppedByEnemy;
+
+    public void init(AttachedItemData attachedItemData, bool droppedByEnemy) {
+        //This will copy over all the values from the base class
+        this.stackable = attachedItemData.stackable;
+        this.XPValue = attachedItemData.XPValue;
+        this.twoDimensionalPrefab = attachedItemData.twoDimensionalPrefab;
+        this.threeDimensionalPrefab = attachedItemData.threeDimensionalPrefab;
+        this.itemType = attachedItemData.itemType;
+        this.itemID = attachedItemData.itemID;
+        this.inventorySlotIDOccupied = attachedItemData.GetInventorySlotIDOccupied();
+
+        //Add the new values for this specific class
+        this.droppedByEnemy = droppedByEnemy;
+    }
+
+    public RawMaterial toRawMaterialClass() {
+        RawMaterial thisRawMaterial = new RawMaterial();
+        thisRawMaterial.init(this);
+        return thisRawMaterial;
+    }
+}
