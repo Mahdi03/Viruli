95ef20465b502f052fb272dbceb94dde1551ab35
Mahdi Ayman <damahdi03@gmail.com>
2022-11-20 17:15:59 -0800

    Publicly defined method to remove all children of GameObject

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,18 @@ public class GameManager : MonoBehaviour {
     private static GameManager instance;
     public static GameManager Instance { get { return instance; } }
 
+    /// <summary>
+    /// Publicly defined method to remove all children of a GameObject (for UI purposes mainly)
+    /// </summary>
+    /// <param name="obj">The GameObject of which you want to clear all children</param>
+    public static void clearAllChildrenOfObj(GameObject obj) {
+        for (int i = obj.transform.childCount - 1; i > -1; i--) {
+            GameObject objToDelete = obj.transform.GetChild(i).gameObject;
+            Destroy(objToDelete);
+        }
+    }
+
+
 
     public GameObject tooltipObjInScene;
 
