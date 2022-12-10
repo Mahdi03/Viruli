f1eed6317b2a15129dca8e5752ed9d34d53e18ba
Mahdi Ayman <damahdi03@gmail.com>
2022-12-10 13:50:59 -0800

    Slowed down game time while crafting UI is open

--- Assets/Scripts/Crafting System/UI Scripts/OpenCraftingMenu.cs
+++ Assets/Scripts/Crafting System/UI Scripts/OpenCraftingMenu.cs
@@ -25,6 +25,7 @@ public class OpenCraftingMenu : MonoBehaviour, IPointerDownHandler {
 	}
 
 	public void showCraftingMenu() {
+		Time.timeScale = 0.25f; //Decrease game speed in the background so that the user can focus more on the crafting UI
 		craftingMenu.SetActive(true);
 		CraftingUIDoorsManager.Instance.resumeCoroutines();
 	}
